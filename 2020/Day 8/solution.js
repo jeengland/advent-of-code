76018ac1e0494df39598c0f10fecaf65ca1096db
unknown <jacob.e.england@gmail.com>
2020-12-08 00:17:46 -0500

    Added solution to day 8 part 1

--- 2020/Day 8/solution.js
+++ 2020/Day 8/solution.js
@@ -1,1 +1,36 @@
 const txtToArray = require('../../utils/txtToArray')
+
+const solution = (input) => {
+    const instructions = txtToArray(input)
+    const cache = {}
+    let acc = 0
+    let i = 0
+    let run = true
+    while (run == true) {
+        if (cache[i]) {
+            run = false
+            break;
+        } else if (i == instructions.length) {
+            return 'Success!'
+        } else {
+            cache[i] = 1
+        }
+        let [inst, amt] = instructions[i].split(' ')
+        amt = parseInt(amt)
+        switch (inst) {
+            case 'acc':
+                acc += amt
+                i++
+                break;
+            case 'jmp':
+                i += amt
+                break;
+            case 'nop':
+                i++;
+                break;
+        }
+    }
+    return acc
+}
+
+console.log(solution('./input.txt'))
